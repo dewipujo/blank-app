8a01e1c58fd9496b7a5a38797ed0e787b8aefaed
dewipujo <kbm.akabo@gmail.com>
2025-04-26 12:26:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
-st.header("KALKULATOR")
-st.write(
-    "Aplikasi ini digunakan untuk melakukan operasi aritmatika sederhana"
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
 )
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
